1114f92c7993076369a5cc1474e1771cb5780361
Lionel Mangoua <bobymangoua@gmail.com>
2024-03-24 16:24:01 +0200

    * Record-play scenario for login page

---
+++ tests/record-login-test.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('student');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Password123');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.getByRole('strong')).toContainText('Congratulations student. You successfully logged in!');
+});
