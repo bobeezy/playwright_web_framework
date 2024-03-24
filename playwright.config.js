9a6290e0eeecde9648d2447a1421ce45b1434296
Lionel Mangoua <bobymangoua@gmail.com>
2024-03-24 16:59:23 +0200

    * Record at cursor scenario

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ module.exports = defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
-    // headless: false,  // Run tests in headful mode.(By opening the browser window)
+    headless: false,  // Run tests in headful mode.(By opening the browser window)
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
